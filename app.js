b0858128c90e6295ddb7843c705d113e3590b40e
Yongcheng Zhu <yzhu002@citymail.cuny.edu>
2019-01-12 20:42:04 +0000

    Finished styling comment section.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app        = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 //------------------------------------------------------------------------------
 //  Database Configurations
